ca49ce8974c4bbe3a31434d18bd7cc02fb71ae48
porl88 <porl88@gmail.com>
2016-01-18 12:11:50 +0100

    Breadcrumb added to all pages

--- MVC.Core/Configuration/RegularExpressions.cs
+++ MVC.Core/Configuration/RegularExpressions.cs
@@ -36,6 +36,14 @@
 
         public const string PasswordWeak = @"^\S{8,}$"; // min 8 characters, no white space characters
 
+        public static string PasswordWeakPattern
+        {
+            get
+            {
+                return PasswordWeak.TrimStart('^').TrimEnd('$');
+            }
+        }
+
         public const string PasswordMedium = @"^(?=(.*[a-z]){2})(?=(.*[A-Z\d\W_]){2})\S{8,}$"; // min 8 characters, no white space, at least 2 lower case characters, must contain at least 2 of ANY combination of the following: uppercase characters OR digits OR special characters (e.g. ~$%#)
 
         public const string PasswordStrong = @"^(?=.*[a-z])(?=.*[A-Z])(?=(.*\d))\S{8,}$"; // min 8 characters, no white space, must contain at least 1 of EACH the following characters: lowercase characters AND uppercase characters AND digits
