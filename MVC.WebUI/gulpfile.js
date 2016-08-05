290fa5b73dadc6ba6830361ca1aec5bd5b04ded2
Paul <paul@waykonect.com>
2016-08-05 22:07:07 +0200

    Further tidying up of SASS files

--- MVC.WebUI/gulpfile.js
+++ MVC.WebUI/gulpfile.js
@@ -6,12 +6,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
+var autoprefixer = require('gulp-autoprefixer'); // https://www.npmjs.com/package/gulp-autoprefixer - does not work with SASS??? need to use Post CSS instead???
 var minifyjs = require('gulp-uglify');
 var concat = require('gulp-concat');
 
+// browserlist config: https://github.com/ai/browserslist#queries
 var autoprefixerOptions = {
-    browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+    browsers: ['Firefox ESR', 'Firefox >= 38', 'IE >= 8', 'Edge >= 12', 'last 4 Chrome versions', 'last 4 Opera versions', 'Safari >= 7', 'IOS >= 7']
 };
 
 gulp.task('minify-js', function () {
@@ -22,12 +23,21 @@ gulp.task('minify-js', function () {
 });
 
 gulp.task('sass-compile', function () {
-    gulp.src('./content/css/scss/styles.scss')
+    gulp.src('./content/website/css/scss/website.scss')
         .pipe(sourcemaps.init())
+        .pipe(autoprefixer(autoprefixerOptions)) // does not work with SASS??? - possibly issue with sourcemaps - might need to call sourcemaps twice: https://github.com/sindresorhus/gulp-autoprefixer/issues/8, https://github.com/floridoo/gulp-sourcemaps/issues/60
         .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(sourcemaps.write('maps'))
-        .pipe(autoprefixer(autoprefixerOptions))
         .pipe(gulp.dest('./content/css'));
+});
+
+gulp.task('default', function () {
+    return gulp.src('src/**/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(autoprefixer())
+        .pipe(concat('all.css'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist'));
 });
 
 // set bindings in Task Runner Explorer to 'project open'
@@ -35,3 +45,28 @@ gulp.task('sass-watch', function () {
     gulp.watch('./content/css/scss/*.scss', ['sass-compile']);
     gulp.watch('./content/css/scss/**/*.scss', ['sass-compile']);
 });
+
+
+
+
+
+
+
+
+
+/* POST CSS */
+/*
+var postcss = require('gulp-postcss'); // https://www.npmjs.com/package/gulp-postcss
+var autoprefixer = require('autoprefixer'); // https://www.npmjs.com/package/autoprefixer, Autoprefixer CSS online: http://autoprefixer.github.io/
+var cssnano = require('cssnano'); // https://www.npmjs.com/package/cssnano
+
+gulp.task('postcss', function () {
+    var processors = [
+        autoprefixer({ browsers: ['Firefox ESR', 'Firefox >= 38', 'IE >= 8', 'Edge >= 12', 'last 4 Chrome versions', 'last 4 Opera versions', 'Safari >= 7', 'IOS >= 7'] }), // browserlist config: https://github.com/ai/browserslist#queries
+        cssnano(),
+    ];
+    return gulp.src('./content/website/css/website.css')
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('./dest'));
+});
+*/
